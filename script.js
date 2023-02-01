5a9b886cccb160031864be70f8993da586fc0943
mikaelribeiro <mikaelribeirors@gmail.com>
2023-01-31 21:28:14 -0300

    Atividade

---
+++ script.js
@@ -0,0 +1,49 @@
+//1 - crie uma função que exiba uma mensagem no console
+//console.log(Olá VaiNaWeb);
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+
+//function exibirNome(nome){
+//   console.log(`Meu nome é ${nome}`)
+//   }
+//    exibirNome("Mikael")
+
+//3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+
+//infos = function (nome , idade, estiloMusical){
+
+//    console.log(`Meu nome é ${nome}`)
+//    console.log(`Minha idade é ${idade}`)
+//    console.log(`Minha idade é ${estiloMusical}`)
+//    }
+//   infos ("Mikael", "20 anos", "Rio de Janeiro")
+
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+
+//infos = function (filme, musica){
+//    console.log(`O melhor filme que eu assisti${filme}`)
+//    console.log(`A melhor música todas é ${musica}`)
+//}
+//infos ("Os vingadores","Minha cura")
+
+//5 - crie uma função que retorne o triplo do número recebido no parâmetro da função
+
+// function triplo (number){
+// return number * 3
+// }
+// console.log(triplo (9))
+
+//6 - crie uma função que  verifique se uma  variável é true ou false
+
+//dados = function (num1, num2){
+//    console.log(`Os números ${num1} e ${num2} são diferentes?`)
+//    if (num1 = num2){
+//       console.log(' Sim')
+//    }else{
+//        console.log('é mentira')
+//    }
+//}
+
+//dados (15,30)
+
+
